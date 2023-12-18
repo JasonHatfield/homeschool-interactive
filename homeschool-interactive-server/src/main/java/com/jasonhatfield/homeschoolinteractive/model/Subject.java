627c95434114ba78219e02f42db04a174c541ca7
Jason Hatfield <gitlab.h8fg@smiplelogin.com>
2023-12-17 21:21:37 -0500

    updated formatting

--- homeschool-interactive-server/src/main/java/com/jasonhatfield/homeschoolinteractive/model/Subject.java
+++ homeschool-interactive-server/src/main/java/com/jasonhatfield/homeschoolinteractive/model/Subject.java
@@ -21,7 +21,7 @@ public class Subject {
 
     @NotNull(message = "Name cannot be null")
     @Size(min = 1, max = 255, message = "Name must be between 1 and 255 characters")
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false)
     private String name;
 
 }
